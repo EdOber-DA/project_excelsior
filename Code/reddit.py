2dd2b13d3b726e5f16e1bc7f1000a3b0df40ae6e
carterkioski <carterkioski@gmail.com>
2021-02-09 20:54:11 -0500

    cleaning up reddit script, added restuls csv

--- Code/reddit.py
+++ Code/reddit.py
@@ -30,26 +30,6 @@ for submission in subreddit.search(query='GME',sort='comments',time_filter='year
     count = 0
     comment_bodies = []
 
-#Running the same test on r/WallStreetBetsNew
-subreddit = reddit.subreddit('wallstreetbetsnew')
-for submission in subreddit.search(query='GME',sort='comments',time_filter='year',limit=None):
-    if submission.created_utc > 1612328400:
-        continue
-    top_level_comments = list(submission.comments)
-    for comment in top_level_comments:
-        #we are only looking at top level comments, so no comments of comments
-        if type(comment) != praw.models.MoreComments:
-            if 'GME' in comment.body:
-                count += 1
-    #takes unix timestamp and converts to datetime object so it is readable
-    date = datetime.fromtimestamp(submission.created_utc).strftime('%m/%d/%Y')
-    if date in comment_counts:
-        comment_counts[date] += count
-    else:
-        comment_counts[date] = count
-    count = 0
-    comment_bodies = []
-
 dates = []
 comments = []
 for k,v in comment_counts.items():
